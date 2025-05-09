679393af679af05e9ee4c0420594965d9b5fc763
fwyr <105589546+fwyr@users.noreply.github.com>
2025-05-10 00:59:28 +0800

    feat: installed threlte.js

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<slot />
+<style>
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+</style>
---
+++ site/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Canvas } from '@threlte/core'
+    import Scene from '$lib/Scene.svelte'
+</script>
+
+
+<main style="width: 100vw; height: 100vh; margin: 0; padding: 0; overflow: hidden;">
+    <Canvas>
+        <Scene />
+    </Canvas>
+</main>
