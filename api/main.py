6788d5052a14f207095a30fc20c5f8abe8cb8bdc
manystarsapart <manystarsapart@gmail.com>
2025-05-27 20:18:59 +0800

    dev: allow for CORS from frontend

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI 
 from .recommendation import * 
 from .extraction import get_requirements
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://rigai-site-chi.vercel.app",
+    "https://www.swearjar.help",
+    "http://localhost:5173" 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 @app.get("/")
 async def root():
