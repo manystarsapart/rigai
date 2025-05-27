fb45303f301e957ddd34b05a6571fdc7d2b7aef3
manystarsapart <manystarsapart@gmail.com>
2025-05-27 15:20:12 +0800

    dev: add all py files into init

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
-# inside main.py
-# from fastapi import FastAPI 
-
-
 from . import recommendation
 from . import extraction
+from . import components
+from . import config
+from . import filters
+from . import utils
+
